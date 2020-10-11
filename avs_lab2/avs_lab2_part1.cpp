fa2d2b989da4fd47b503dd77784ee6b932b88be6
Seleo99 <39170907+Seleo99@users.noreply.github.com>
2020-10-12 00:56:51 +0300

    Add files via upload

--- avs_lab2/avs_lab2_part1.cpp
+++ avs_lab2/avs_lab2_part1.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <pthread.h>
 #include <chrono>
+#include <ctime>
 #include <mutex>
 #include <atomic>
 
@@ -18,8 +19,7 @@ class CounterMutex {
 
         int get_value(){
             std::lock_guard<std::mutex> lg(mtx);
-            count_++;
-            return count_;
+            return count_++;
         }
 };
 
@@ -31,20 +31,18 @@ public:
     }
 
     int get_value(){
-        count_++;
-        //std::cout<<count_<<" ";
-        return count_;
+        return count_++;
     }
 };
 
-class Task1_lab2{
+class BigArray{
     private:
         size_t num_tasks_;
     public:
         int * array;
-        Task1_lab2(int numTasks): num_tasks_(numTasks){
+        BigArray(size_t numTasks): num_tasks_(numTasks){
             array = new int[numTasks]();
-            for (int i = 0; i < numTasks; ++i){
+            for (size_t i = 0; i < numTasks; ++i){
                 array[i] = 0;
             }
         }
@@ -53,71 +51,78 @@ class Task1_lab2{
             std::cout<<std::endl;
             for (size_t i = 0; i < num_tasks_; ++i) {
                 if (array[i] == 0) {
-                    return "not ok\n";
+                    return "test: not ok\n";
                 }
             }
-            return "ok\n";
+            return "test: ok\n";
         }
 
         size_t size() const{
-            std::lock_guard<std::mutex> lg(mtx);
+            //std::lock_guard<std::mutex> lg(mtx);
             return num_tasks_;
         }
-        Task1_lab2(Task1_lab2 &&) = default;
+        BigArray(BigArray &&) = default;
         int& operator[] (std::size_t index);
-        ~Task1_lab2(){
+        ~BigArray(){
             delete [] array;
         }
 
 };
 
-int& Task1_lab2::operator[] (std::size_t index)
+int& BigArray::operator[] (std::size_t index)
 {
     //std::lock_guard<std::mutex> lg(mtx2);
-    return array[index];
+    return const_cast<int &>(array[index]);
 }
 
 template<typename T>
-void start_thread(T &c, Task1_lab2 &task1){
+void start_thread(T &c, BigArray &task1){
+
     while(true){
         int tmp = c.get_value();
         if (tmp >= task1.size())
-            return;
-        //std::this_thread::sleep_for(std::chrono::nanoseconds(10));
+            break;
+        std::this_thread::sleep_for(std::chrono::nanoseconds(10));
         task1[tmp] = 1;
-        std::cout<<tmp<<" ";
     }
+
 }
 
 int main() {
     std::vector<uint> nums_threads {4, 8, 16, 32}; //, 8, 16, 32
     std::vector<std::thread> vec_threads;
-    constexpr int num_tasks = 1000; //1024*1024
-    std::cout<<"using std::mutex: \n";
+    constexpr int num_tasks = 1024*1024;
+    std::cout<<"\nusing std::mutex: \n\n";
     for(uint &num: nums_threads){
-        Task1_lab2 task1(num_tasks);
+        BigArray task1(num_tasks);
         CounterMutex c;
         std::cout<<"for NumThrears = "<<num<<std::endl;
+        unsigned int start_time =  clock();
         for (uint i = 0; i < num; ++i) {
             vec_threads.push_back(std::thread(start_thread<CounterMutex>, std::ref(c), std::ref(task1)));
         }
+
         for (auto& thr: vec_threads){
             thr.join();
         }
+        std::cout<<"time: "<<clock() - start_time;
         std::cout<<task1.test_array()<<std::endl;
         vec_threads.clear();
     }
-    std::cout<<"using std::atomic: \n";
+    std::cout<<"\nusing std::atomic: \n";
     for(uint &num: nums_threads){
-        Task1_lab2 task1(num_tasks);
+        BigArray task1(num_tasks);
         CounterAtomic c;
         std::cout<<"for NumThrears = "<<num<<std::endl;
+
         for (uint i = 0; i < num; ++i) {
             vec_threads.push_back(std::thread(start_thread<CounterAtomic>, std::ref(c), std::ref(task1)));
         }
+        unsigned int start_time =  clock();
         for (auto& thr: vec_threads){
             thr.join();
         }
+        std::cout<<"time: "<<clock() - start_time<<std::endl;
         std::cout<<task1.test_array();
         vec_threads.clear();
     }
