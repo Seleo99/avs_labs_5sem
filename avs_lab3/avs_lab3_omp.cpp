2fc0cfcd6c933f6adcc328babc559c40cb7e0951
Seleo99 <39170907+Seleo99@users.noreply.github.com>
2020-11-14 03:08:09 +0300

    Add files via upload

--- avs_lab3/avs_lab3_omp.cpp
+++ avs_lab3/avs_lab3_omp.cpp
@@ -4,21 +4,23 @@
 #include <random>
 #include <algorithm>
 #include <iterator>
+#include <iomanip>
 
 //вариант 12 - свертка двух последовательностей
-
 //Через преобразования Фурье можно за 3*n*logn + n накодить))))
-// Размер выходной последовательности равен M + N - 1
+
 template<typename T>
 void print(std::vector<T> const &vc){
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(3);      // до 6 включит
     for (T const &el: vc){
-        std::cout<<el;
+        std::cout<<std::setw(25)<<std::left<<el<<" ";
     }
     std::cout<<std::endl;
 }
 
 template<typename T>
-std::vector<double> randomVector(size_t const &size)
+std::vector<T> randomVector(size_t const &size)
 {
     std::vector<T> v(size);
     std::random_device r;
@@ -27,8 +29,8 @@ std::vector<double> randomVector(size_t const &size)
 }
 
 template<typename T>
-const std::vector<T> convolution(std::vector<T> const &seq1, std::vector<T> const & seq2) {
-    std::vector<T> result(seq1.size() + seq2.size() - 1);
+const std::vector<T> convolution(std::vector<T> const &seq1, const size_t size_seq1, std::vector<T> const & seq2, const size_t size_seq2) {
+    std::vector<T> result(size_seq1 + size_seq2 - 1, 0);
     for (size_t i = 0; i < result.size(); ++i) {
         for (size_t j = 0; j < seq2.size(); ++j) {
             if (i - j < seq1.size()) {
@@ -40,12 +42,20 @@ const std::vector<T> convolution(std::vector<T> const &seq1, std::vector<T> cons
 }
 
 int main() {
-    std::vector<double> seq1(randomVector<double>(1024)), seq2(randomVector<double>(1024));
-    std::vector<double> res = convolution(seq1, seq2);
+    const size_t size_seq1 = 3;
+    const size_t size_seq2 = 2;
+    //std::vector<double> seq1(randomVector<double>(size_seq1)), seq2(randomVector<double>(size_seq2));
+    std::vector<double> seq1{2, -2, 1};
+    std::vector<double> seq2{1, 2};
+    //дополняем нулями
+    std::vector<double> zeros1(size_seq2 - 1, 0), zeros2(size_seq1 - 1, 0);
+    seq1.insert(seq1.end(), std::make_move_iterator(zeros1.begin()), std::make_move_iterator(zeros1.end()));
+    seq2.insert(seq2.end(), std::make_move_iterator(zeros2.begin()), std::make_move_iterator(zeros2.end()));
     std::cout << "Seq1: ";
     print(seq1);
     std::cout << "Seq2: ";
     print(seq2);
+    std::vector<double> res = convolution(seq1, size_seq1, seq2, size_seq2);
     std::cout<<"Resl: ";
     print(res);
     return 0;
